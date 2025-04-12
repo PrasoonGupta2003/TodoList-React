042c9647488d611cf869ecfc8c266de31cc7cfa0
PrasoonGupta2003 <praveengupta1969.pg@gmail.com>
2025-04-12 11:59:52 +0530

    FrontEnd Todo list React

--- To-Do-List/src/App.jsx
+++ To-Do-List/src/App.jsx
@@ -4,6 +4,8 @@ import TodoList from './TodoList';
 function App() {
   return (
     <>
+      <h1 style={{color:"#3BAFDA",fontWeight:"900"}}><i>"Conquer the chaos—one checkbox at a time."</i></h1>
+      <p style={{color:"#1E1E2F",fontWeight:"700", fontSize:"30px"}}>"Welcome back, Boss. Let’s turn plans into progress." </p>
       <TodoList/>
     </>
   )
